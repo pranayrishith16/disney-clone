73e55c2a8da5c0932117e64124d1b2590e45eec4
pranay <pranayrishith007@gmail.com>
2022-04-16 19:12:43 +0530

    working....

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,33 @@ import styled from 'styled-components'
 
 const Home = (props) => {
   return (
-    <div>Home</div>
+    <Container>
+        <h2>Hello</h2>
+        <h2>Hello</h2>
+        <h2>Hello</h2>
+        <h2>Hello</h2>
+        <h2>Hello</h2>
+        <h2>Hello</h2>
+    </Container>
   )
 }
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh-250px);
+    overflow-x: hidden;
+    display: block;
+    top: 57px;
+    padding: 0 calc(3vw + 5px);
+
+    &:after {
+        background: url('/images/home-background.png') center center / cover no-repeat fixed ;
+        content: "";
+        position: absolute;
+        inset: 0;
+        opacity: 1;
+        z-index: -1;
+    }
+`
 
 export default Home
