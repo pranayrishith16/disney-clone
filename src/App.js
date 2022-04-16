b2e26c0c0a9d45082b778bea33507812bc3928de
pranay <pranayrishith007@gmail.com>
2022-04-16 18:44:28 +0530

    Working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 import Login from './components/Login'
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route exact path='/' element={<Login />}>
-          </Route>
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/home' element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,70 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import {auth, provider} from './Firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {
+    selectUserEmail,
+    selectUserName,
+    selectUserPhoto,
+    setSignOutState,
+    setUserLoginDetails
+} from '../features/user/userSlice'
 
-const Header = () => {
+const Header = (props) => {
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const userName = useSelector(selectUserName)
+    const userPhoto = useSelector(selectUserPhoto);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                setUser(user)
+                history('/home')
+            }
+        })
+    },[userName])
 
     const handleAuth = () => {
+        if (!userName){
+
         auth.signInWithPopup(provider)
         .then((result) => {
-            console.log(result)
+            setUser(result.user);
         })
         .catch((error) => {
-            alert(error.message)
+            console.log((error.message))
         })
+        }else if (userName){
+            auth.signOut()
+            .then(() => {
+                dispatch(setSignOutState())
+                history('/')
+            }).catch((error) => console.log(error.message))
+        }
+
     }
+
+    const setUser = (user) => {
+        dispatch(setUserLoginDetails({
+            name:user.displayName,
+            email:user.email,
+            photo:user.photoURL,
+        }))
+    }
+
+    
 
   return (
     <Nav>
         <Logo>
             <img src='images/logo.svg' alt='disney'/>
         </Logo>
+        {
+            !userName ? 
+            <Login onClick={handleAuth}>LOGIN</Login> :
+        <>
         <NavMenu>
             <a href='/home'>
                 <img src='images/home-icon.svg' alt='home'/>
@@ -45,7 +91,14 @@ const Header = () => {
                 <span>SERIES</span>
             </a>
         </NavMenu>
-        <Login onClick={handleAuth}>LOGIN</Login>
+        <Menu>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+                <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+        </Menu>
+        </>
+    }
     </Nav>
   )
 }
@@ -158,6 +211,50 @@ const Login = styled.a`
         background-color: #f9f9f9;
         color: #000;
         border-color: transparent;
+    }
+`
+
+const UserImg = styled.img`
+    height: 100%;
+`
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0;
+    background: rgb(19,19,19);
+    border: 1px solid rgba(151,151,151,0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 /50%) 0 0 18px 0;
+    padding: 9px;
+    font-size: 14px;
+    letter-spacing: 2px;
+    width: 110px;
+    opacity: 0;
+`
+
+
+
+const Menu = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg}{
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
     }
 `
 
